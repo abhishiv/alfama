e79c696e52239c20ede8472b73c4c37ec45c0cb5
Abhishiv Saxena <abhishiv@gmail.com>
2024-10-16 11:26:24 +0530

    rm stale files

--- src/addons/animation/index.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentUtils } from "../../dom";
-
-export const createAnimation = <T>(
-  utils: ComponentUtils,
-  initialValue: any
-) => {
-  const $signal = utils.signal("animation", initialValue);
-  return [$signal];
-};
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,24 @@
+export type * from "./core/state";
 export {
-  component,
-  h,
-  render,
-  unrender,
-  Fragment,
-  ParentWireContext,
-  defineContext,
-} from "./dom";
-export type * from "./dom";
-export {
-  createSignal,
-  createWire,
-  createStore,
   createComputedSignal,
+  createSignal,
+  createStore,
+  createWire,
+  getProxyMeta,
   produce,
   reify,
-  getProxyMeta,
 } from "./core/state";
-export type * from "./core/state";
+export type * from "./dom";
+export {
+  component,
+  defineContext,
+  Fragment,
+  h,
+  ParentWireContext,
+  render,
+  unrender,
+} from "./dom";
 export * from "./stdlib/index";
 export { getValueUsingPath } from "./utils/index";
 
-import { Signal } from "./core";
-import { ComponentUtils } from "./dom";
-
 export { getCursor } from "./utils/index";
-
-export type UsePromiseResp<T = unknown, E = unknown> = {
-  $data: Signal<T | undefined>;
-  $error: Signal<E | undefined>;
-  $loading: Signal<boolean | undefined>;
-  $status: Signal<"loading" | "loaded" | "failed">;
-};
